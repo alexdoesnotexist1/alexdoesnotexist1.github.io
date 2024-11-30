bb74370418e7407dbfd67d096d9021496b5c46df
alexdoesnotexist1 <alexanderharmonic@gmail.com>
2024-11-30 00:52:05 -0500

    debug 1

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-react-app/',
+  build: {
+    outDir: 'dist',            // The build folder that will be deployed
+  },
   plugins: [react()],
 })
